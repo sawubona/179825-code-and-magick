10b5e0d43b4349973110626450414018446c01aa
MaxFedotoff <sawubona36@gmail.com>
2017-11-27 11:31:10 +0300

    Исправил замечания

--- js/stat.js
+++ js/stat.js
@@ -7,48 +7,46 @@ var renderStatistics = (ctx, names, times) => {
     ctx.fillRect(100, 10, 420, 270);
     ctx.fillStyle = '#000';
     ctx.font = '16px PT Mono';
-    ctx.fillText('Ура вы победили!', 120, 40);
     ctx.fillText('Список результатов:', 120, 60);
 
     const sortingTimes = times.sort();
-    for (let i = 0; i < names.length; i++) {
-        const name = names[i];
-        const gamers = new Gamer(name, sortingTimes[i], i);
-        gamers.render();
-    };
 
-    function Gamer(name, time, index) {
-        this.index = index + 1;
-        this.name = name;
-        this.score = time.toFixed();
-
+    let renderHistogram = (gamertime, gamername, index) => {
+        const maxTime = Math.max.apply(null, sortingTimes).toFixed();
+        const minTime = Math.min.apply(null, sortingTimes).toFixed();
         const maxHistogramHeight = 150;
         const histogramWidth = 40;
         const startY = 250;
         const startX = 65;
         const columnDistance = 50;
-        const stepY = maxHistogramHeight/(Math.max.apply(null, times)-0);
-        let stepX = startX + (this.index * (columnDistance + histogramWidth));
-        let histogramHeight = this.score * stepY;
+        const stepY = maxHistogramHeight / (maxTime - 0);
+        let stepX = startX + (index * (columnDistance + histogramWidth));
+        let histogramHeight = gamertime * stepY;
 
         if (histogramHeight > maxHistogramHeight) {
             histogramHeight = maxHistogramHeight;
-        };
-
-        this.render = () => {
-            if (this.name === 'Вы') {
-                ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-                ctx.fillRect(stepX, startY, histogramWidth, - histogramHeight);
-                ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-                ctx.fillText(this.name, stepX, 270);
-                ctx.fillText(this.score, stepX, startY - histogramHeight - 10);
-            } else {
-                ctx.fillStyle = (`rgba(0, 0, 139, ${(maxHistogramHeight/this.score)*10}`);
-                ctx.fillRect(stepX, startY, histogramWidth, - histogramHeight);
-                ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-                ctx.fillText(this.name, stepX, 270);
-                ctx.fillText(this.score, stepX, startY - histogramHeight - 10);
-            }
         }
+        if (gamername === 'Вы') {
+            ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+            ctx.fillRect(stepX, startY, histogramWidth, - histogramHeight);
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+            ctx.fillText(gamername, stepX, 270);
+            ctx.fillText(gamertime, stepX, startY - histogramHeight - 10);
+            if (gamertime <= minTime) {
+                ctx.fillStyle = '#000';
+                ctx.font = '16px PT Mono';
+                ctx.fillText('Ура вы победили!', 120, 40);
+            }
+        } else {
+            ctx.fillStyle = (`rgba(0, 0, 139, ${(maxHistogramHeight / gamertime) * 10}`);
+            ctx.fillRect(stepX, startY, histogramWidth, - histogramHeight);
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+            ctx.fillText(gamername, stepX, 270);
+            ctx.fillText(gamertime, stepX, startY - histogramHeight - 10);
+        }
+    };
+
+    for (let i = 0; i < sortingTimes.length; i++) {
+        renderHistogram(sortingTimes[i].toFixed(), names[i], i + 1);
     };
 };
